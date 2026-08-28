7cc33fa70391c94a00bd2ec0fcea0ee45e96dd99
Nando Luz <nando.sousa@poupex.com.br>
2026-08-28 18:07:49 -0300

    feat: Notification Pattern na Entidade Product

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -1,33 +1,125 @@
+import { NotificationErrorProps } from "../../@shared/notification/notification";
+import NotificationError from "../../@shared/notification/notification.error";
 import Product from "./product";
+
+const captureNotificationErrors = (
+  createProduct: () => void
+): NotificationErrorProps[] => {
+  try {
+    createProduct();
+  } catch (error) {
+    return (error as NotificationError).errors;
+  }
+  return [];
+};
 
 describe("Product unit tests", () => {
   it("should throw error when id is empty", () => {
     expect(() => {
-      const product = new Product("", "Product 1", 100);
-    }).toThrowError("Id is required");
+      new Product("", "Product 1", 100);
+    }).toThrowError("product: Id is required");
   });
 
   it("should throw error when name is empty", () => {
     expect(() => {
-      const product = new Product("123", "", 100);
-    }).toThrowError("Name is required");
+      new Product("123", "", 100);
+    }).toThrowError("product: Name is required");
   });
 
   it("should throw error when price is less than zero", () => {
     expect(() => {
-      const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
+      new Product("123", "Name", -1);
+    }).toThrowError("product: Price must be greater than zero");
+  });
+
+  it("should notify both errors when name is empty and price is less than zero", () => {
+    const errors = captureNotificationErrors(() => {
+      new Product("123", "", -1);
+    });
+
+    expect(errors.length).toBe(2);
+    expect(errors).toEqual([
+      { context: "product", message: "Name is required" },
+      { context: "product", message: "Price must be greater than zero" },
+    ]);
+
+    expect(() => {
+      new Product("123", "", -1);
+    }).toThrowError(
+      "product: Name is required,product: Price must be greater than zero"
+    );
+  });
+
+  it("should notify every error when id, name and price are invalid", () => {
+    const errors = captureNotificationErrors(() => {
+      new Product("", "", -1);
+    });
+
+    expect(errors.length).toBe(3);
+    expect(errors).toEqual([
+      { context: "product", message: "Id is required" },
+      { context: "product", message: "Name is required" },
+      { context: "product", message: "Price must be greater than zero" },
+    ]);
+  });
+
+  it("should have an empty notification when the product is valid", () => {
+    const product = new Product("123", "Product 1", 100);
+
+    expect(product.notification.hasErrors()).toBe(false);
+    expect(product.notification.getErrors()).toEqual([]);
+    expect(product.notification.messages()).toBe("");
+  });
+
+  it("should notify an error when changing the name to an empty value", () => {
+    const product = new Product("123", "Product 1", 100);
+
+    const errors = captureNotificationErrors(() => {
+      product.changeName("");
+    });
+
+    expect(errors).toEqual([
+      { context: "product", message: "Name is required" },
+    ]);
+  });
+
+  it("should notify an error when changing the price to a value less than zero", () => {
+    const product = new Product("123", "Product 1", 100);
+
+    const errors = captureNotificationErrors(() => {
+      product.changePrice(-1);
+    });
+
+    expect(errors).toEqual([
+      { context: "product", message: "Price must be greater than zero" },
+    ]);
+  });
+
+  it("should not accumulate errors from previous validations", () => {
+    const product = new Product("123", "Product 1", 100);
+
+    expect(() => {
+      product.changePrice(-1);
+    }).toThrowError("product: Price must be greater than zero");
+
+    const errors = captureNotificationErrors(() => {
+      product.changePrice(-1);
+    });
+
+    expect(errors.length).toBe(1);
   });
 
   it("should change name", () => {
     const product = new Product("123", "Product 1", 100);
     product.changeName("Product 2");
     expect(product.name).toBe("Product 2");
+    expect(product.notification.hasErrors()).toBe(false);
   });
 
   it("should change price", () => {
     const product = new Product("123", "Product 1", 100);
     product.changePrice(150);
     expect(product.price).toBe(150);
+    expect(product.notification.hasErrors()).toBe(false);
   });
 });
--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,21 +1,22 @@
+import Entity from "../../@shared/entity/entity.abstract";
+import Notification from "../../@shared/notification/notification";
+import NotificationError from "../../@shared/notification/notification.error";
+import ProductValidatorFactory from "../factory/product.validator.factory";
 import ProductInterface from "./product.interface";
 
-export default class Product implements ProductInterface {
-  private _id: string;
+export default class Product extends Entity implements ProductInterface {
   private _name: string;
   private _price: number;
 
   constructor(id: string, name: string, price: number) {
+    super();
     this._id = id;
     this._name = name;
     this._price = price;
     this.validate();
+    this.throwIfInvalid();
   }
 
-  get id(): string {
-    return this._id;
-  }
-  
   get name(): string {
     return this._name;
   }
@@ -27,23 +28,25 @@ export default class Product implements ProductInterface {
   changeName(name: string): void {
     this._name = name;
     this.validate();
+    this.throwIfInvalid();
   }
 
   changePrice(price: number): void {
     this._price = price;
     this.validate();
+    this.throwIfInvalid();
   }
 
-  validate(): boolean {
-    if (this._id.length === 0) {
-      throw new Error("Id is required");
+  validate(): void {
+    // Cada validacao parte de uma notificacao limpa, para que chamadas
+    // sucessivas nao acumulem erros de estados ja corrigidos.
+    this.notification = new Notification();
+    ProductValidatorFactory.create().validate(this);
+  }
+
+  private throwIfInvalid(): void {
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.getErrors());
     }
-    if (this._name.length === 0) {
-      throw new Error("Name is required");
-    }
-    if (this._price < 0) {
-      throw new Error("Price must be greater than zero");
-    }
-    return true;
   }
 }
